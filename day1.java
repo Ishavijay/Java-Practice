c36616fc556271b10eb69c80cf73303f3de13338
ishavjay <ishavijay003@gmail.com>
2026-07-24 19:12:46 +0530

    Practice

--- day1.java
+++ day1.java
@@ -1,7 +1,22 @@
 public class day1{
         public class customer {
             private String firstname;
-            private int age;
+            private int Age;
              private String lastname;
+             public String getLastName(){ //getter method
+                    return lastname;
+}
+public void setLastname(String Lastname){
+    this.lastname=Lastname;
+}}
+public class day{
+    public static void main(String[] args) {
+        System.out.println("I am Isha");// to print 
     }
 }
+}
+//two types of methods are there getter and setter
+//getter method return the last name inserted
+//setter method is used to set the variable value.
+//In this customer will act as a data type and we will be able to define new variable using this datatype and id we want to extract any method we can do this by using .nameofmethod
+//inheritance done using keyword extends e.g 
---
+++ inheritance.java
@@ -0,0 +1,9 @@
+public class inheritance {
+    public class product{
+
+    }
+    public class customer extends product{ // Extend ke baad jo likhte usse inherit karega, Extend se pehle wali inherit karegi
+
+    }
+    
+}
